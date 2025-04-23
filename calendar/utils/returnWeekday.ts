d86f2d88ce6b8241d56078f1f060e275a1e6d682
Ola Hagen Bendixen <olaben@afk.no>
2025-04-23 10:57:11 +0200

    fix: fix checks on if dates exist, add message to indicate what has gone wrong or right

--- calendar/utils/returnWeekday.ts
+++ calendar/utils/returnWeekday.ts
@@ -10,8 +10,12 @@ export default function returnWeekday(day: number, month: number, year: number):
    //Output = an integer that can be mapped to determine the weekday
 
    //Determine what calender "type" is used (Gregorian, Julian or Fictive)
+
+   console.log("Input: ", day, month, year);
+
    let calenderType: FictiveCalendarCalendarSystemMap;
 
+   let message = "Invalid date";
    let dateExists = false;
    let calenderTypeInteger = 0;
 
@@ -20,48 +24,68 @@ export default function returnWeekday(day: number, month: number, year: number):
 
       if (year % 4 === 0) {
          if (month === 2 && day > 29) {
+            message = "Invalid date: February cannot have more than 29 days in a leap year.";
             dateExists = false;
          } else if (month === 2 && day <= 29) {
+            message = "Valid date: February has 29 days in a leap year.";
             dateExists = true;
          }
       } else {
          if (month === 2 && day > 28) {
+            message = "Invalid date: February cannot have more than 28 days in a non-leap year.";
             dateExists = false;
          } else if (month === 2 && day <= 28) {
+            message = "Valid date: February has 28 days in a non-leap year.";
             dateExists = true;
          } else if (day > 31) {
+            message = "Invalid date: Month cannot have more than 31 days.";
             dateExists = false;
          } else if (day === 31) {
             if (month === 4 || month === 6 || month === 9 || month === 11) {
+               message = "Invalid date: Month cannot have more than 30 days.";
                dateExists = false;
             } else {
+               message = "Valid date: Month has 31 days.";
                dateExists = true;
             }
+         } else {
+            message = "Valid date";
+            dateExists = true;
          }
       }
 
    } else if ((year >= -45 && year < 1582) || (year === 1582 && month <= 10 && day <= 4)) {
-      calenderTypeInteger = 1; //Julian
+      calenderTypeInteger = 1; // Julian
       
       if (year % 4 === 0) {
          if (month === 2 && day > 29) {
+            message = "Invalid date: February cannot have more than 29 days in a leap year.";
             dateExists = false;
          } else if (month === 2 && day <= 29) {
+            message = "Valid date: February has 29 days in a leap year.";
             dateExists = true;
          }
       } else {
          if (month === 2 && day > 28) {
+            message = "Invalid date: February cannot have more than 28 days in a non-leap year.";
             dateExists = false;
          } else if (month === 2 && day <= 28) {
+            message = "Valid date: February has 28 days in a non-leap year.";
             dateExists = true;
          } else if (day > 31) {
+            message = "Invalid date: Month cannot have more than 31 days.";
             dateExists = false;
          } else if (day === 31) {
             if (month === 4 || month === 6 || month === 9 || month === 11) {
+               message = "Invalid date: Some months cannot have more than 31 days.";
                dateExists = false;
             } else {
+               message = "Valid date: Month has 31 days.";
                dateExists = true;
             }
+         } else {
+            message = "Valid date";
+            dateExists = true;
          }
       }
 
@@ -71,28 +95,39 @@ export default function returnWeekday(day: number, month: number, year: number):
       // TODO: year 0 does not exist in Gregorian calendar, but it is not handled here. Sjekk oppgave tekst
 
       if (year === 0) {
+         message = "Invalid date: Year 0 does not exist in the Gregorian calendar.";
          dateExists = false;
       }
 
       if (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) {
          if (month === 2 && day > 29) {
+            message = "Invalid date: February cannot have more than 29 days in a leap year.";
             dateExists = false;
          } else if (month === 2 && day <= 29) {
+            message = "Valid date: February has 29 days in a leap year.";
             dateExists = true;
          }
       } else {
          if (month === 2 && day > 28) {
+            message = "Invalid date: February cannot have more than 28 days in a non-leap year.";
             dateExists = false;
          } else if (month === 2 && day <= 28) {
+            message = "Valid date: February has 28 days in a non-leap year.";
             dateExists = true;
          } else if (day > 31) {
+            message = "Invalid date: Month cannot have more than 31 days.";
             dateExists = false;
          } else if (day === 31) {
             if (month === 4 || month === 6 || month === 9 || month === 11) {
+               message = "Invalid date: Month cannot have more than 30 days.";
                dateExists = false;
             } else {
+               message = "Valid date: Month has 31 days.";
                dateExists = true;
             }
+         } else {
+            message = "Valid date";
+            dateExists = true;
          }
       }
    } else {
@@ -100,8 +135,10 @@ export default function returnWeekday(day: number, month: number, year: number):
          Weekday: null,
          Calendar: null,
          Exists: false,
+         Message: message,
       }
 
+      console.log("Return data in returnWeekday: ", returnData);
       return returnData;
    }
 
@@ -124,15 +161,18 @@ export default function returnWeekday(day: number, month: number, year: number):
             (day + Math.floor((13 * (month + 1)) / 5) + year + Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400) + 6) % 7;
          weekday = mapGregorianWeekday(weekdayInteger);
          calenderType = FictiveCalendarCalendarSystemMap.Gregorian;
+         console.log("data in switch case", weekdayInteger, weekday, calenderTypeInteger, calenderType);
          break;
       default:
          throw new Error("Invalid calendar type");
    }
 
+   console.log("return data line 160: ", weekday, calenderType, dateExists, message);
    const returnData: DateResponse = {
       Weekday: dateExists ? weekday : FictiveCalendarCalendarSystemMap.Unknown,
       Calendar: dateExists ? calenderType : FictiveCalendarCalendarSystemMap.Unknown,
       Exists: dateExists,
+      Message: message,
    };
 
    return returnData;
